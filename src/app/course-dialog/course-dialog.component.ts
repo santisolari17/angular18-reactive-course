c98b40a66b9da24ec31fc90cf216f2a45e003712
santisolari17 <s17manutd@gmail.com>
2024-07-12 02:34:59 +0000

    error handling final

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -6,12 +6,15 @@ import moment from 'moment';
 
 import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../loading/loading.service';
+import { MessagesService } from '../messages/messages.service';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'course-dialog',
   templateUrl: './course-dialog.component.html',
   styleUrls: ['./course-dialog.component.css'],
-  providers: [LoadingService],
+  providers: [LoadingService, MessagesService],
 })
 export class CourseDialogComponent implements AfterViewInit {
   public form: FormGroup;
@@ -22,7 +25,8 @@ export class CourseDialogComponent implements AfterViewInit {
     private dialogRef: MatDialogRef<CourseDialogComponent>,
     @Inject(MAT_DIALOG_DATA) course: Course,
     private coursesService: CoursesService,
-    private loadingService: LoadingService
+    private loadingService: LoadingService,
+    private messagesService: MessagesService
   ) {
     this.course = course;
 
@@ -38,7 +42,14 @@ export class CourseDialogComponent implements AfterViewInit {
 
   save() {
     const changes = this.form.value;
-    const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes);
+    const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes).pipe(
+      catchError((err) => {
+        const message = 'could not save course';
+        console.log(message, err);
+        this.messagesService.showErrors(message);
+        return throwError(err);
+      })
+    );
 
     this.loadingService.showLoaderUntilCompleted(saveCourse$).subscribe((response) => this.dialogRef.close(response));
   }
