e555392d0ac54c7aaeb5740c5e05a9c5006f069f
santisolari17 <s17manutd@gmail.com>
2024-07-12 04:04:04 +0000

    section 5 done

--- src/app/search-lessons/search-lessons.component.ts
+++ src/app/search-lessons/search-lessons.component.ts
@@ -1,48 +1,32 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from '../model/course';
-import {
-  debounceTime,
-  distinctUntilChanged,
-  startWith,
-  tap,
-  delay,
-  map,
-  concatMap,
-  switchMap,
-  withLatestFrom,
-  concatAll, shareReplay
-} from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat} from 'rxjs';
-import {Lesson} from '../model/lesson';
-
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Lesson } from '../model/lesson';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'course',
   templateUrl: './search-lessons.component.html',
-  styleUrls: ['./search-lessons.component.css']
+  styleUrls: ['./search-lessons.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchLessonsComponent implements OnInit {
+  searchResults$: Observable<Lesson[]>;
 
-  constructor() {
+  activeLesson: Lesson;
 
+  constructor(private coursesService: CoursesService) {}
 
+  ngOnInit() {}
+
+  onSearch(search: string) {
+    this.searchResults$ = this.coursesService.searchLessons(search);
   }
 
-  ngOnInit() {
-
-
+  openLesson(lesson: Lesson) {
+    this.activeLesson = lesson;
   }
 
+  onBackToSearch() {
+    this.activeLesson = null;
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,21 +1,60 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Course } from '../model/course';
+import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Lesson } from '../model/lesson';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root',
+})
 export class CoursesService {
   constructor(private http: HttpClient) {}
 
-  public loadAllCourses(): Observable<Course[]> {
-    return this.http.get<{ payload: Course[] }>('/api/courses').pipe(
-      map((response) => response.payload),
+  loadCourseById(courseId: number) {
+    return this.http
+      .get<Course>(`/api/courses/${courseId}`)
+      .pipe(shareReplay());
+  }
+
+  loadAllCourseLessons(courseId: number): Observable<Lesson[]> {
+    return this.http
+      .get<Lesson[]>('/api/lessons', {
+        params: {
+          pageSize: '10000',
+          courseId: courseId.toString(),
+        },
+      })
+      .pipe(
+        map((res) => res['payload']),
+        shareReplay()
+      );
+  }
+
+  loadAllCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>('/api/courses').pipe(
+      map((res) => res['payload']),
       shareReplay()
     );
   }
 
-  public saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
-    return this.http.put(`/api/courses/${courseId}`, changes).pipe(shareReplay());
+  saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
+    return this.http
+      .put(`/api/courses/${courseId}`, changes)
+      .pipe(shareReplay());
+  }
+
+  searchLessons(search: string): Observable<Lesson[]> {
+    return this.http
+      .get<Lesson[]>('/api/lessons', {
+        params: {
+          filter: search,
+          pageSize: '100',
+        },
+      })
+      .pipe(
+        map((res) => res['payload']),
+        shareReplay()
+      );
   }
 }
